1929ca9b55574661b925f412745a93e30a6c4833
Muntaha2436 <hussainkazium592@gmail.com>
2025-09-02 08:52:00 -0700

    assignment 2 done

---
+++ assignment 2/app.js
@@ -0,0 +1,41 @@
+//question: 1
+// var userNAME;
+
+//question: 2
+// var fullNAME = "muntaha"
+
+//question: 3
+// var message ;
+// var message = "hello world"
+// alert(message)
+
+//question: 4
+// var studentNAME = "muntaha sheraz"
+// var studentAGE = "16 years"
+// var studentCLASS = "11 grade"
+// var studentCOURSE = "web and app devlopment"
+
+// alert(studentNAME)
+// alert(studentAGE)
+// alert(studentCLASS)
+// alert(studentCOURSE)
+
+//question: 5
+// var wellcome = "wellcome to all on javascript land"
+// alert(wellcome)
+
+//question: 6
+// var myEmailaddres = "muntahakazim123@gmail.com"
+// alert(myEmailaddres)
+
+//question: 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am learning from the book.... "+book );
+
+//question: 8
+// var write = "yah!i can write HTML content throught javascript"
+// document.write(write)
+
+//question : 9
+// alert(write)
+
