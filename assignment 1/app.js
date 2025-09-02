670d7859f7f2b7161ab89c04d2fc111d89be2611
Muntaha2436 <hussainkazium592@gmail.com>
2025-09-02 07:20:47 -0700

    javascript assignment

---
+++ assignment 1/app.js
@@ -0,0 +1,15 @@
+//qustion :1
+//  alert("Welcome to our website!")
+    
+ //question : 2
+//  alert("earror pleas enter a valid password")
+
+ //question: 3
+//  alert("wellcome to js land  \n happy coding")
+
+ //qustion :4
+//  alert("wellcome to js land")
+//  alert("hello user")
+
+ //question: 5
+//  console.log("hello ... i can web broswer console")
