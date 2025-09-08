e61cb74a0f7309a8f3625171a9254a44cff1ebd8
Muntaha2436 <hussainkazium592@gmail.com>
2025-09-08 05:04:15 -0700

    my work is completed

---
+++ assignment 3/app.js
@@ -0,0 +1,20 @@
+
+// question 1
+
+// let age = "i am 15 years old"
+// alert(age)
+
+// question 2
+
+// let site = "you have visiting this site 20 times"
+// alert(site)
+
+// question 3
+
+// let birth= "my birthday year is 2009"
+// document.write(birth)
+
+// question 4
+
+// let order ="john deard ordered in 5 T-shirt(s) on xyz clothing store "
+// document.write(order)
