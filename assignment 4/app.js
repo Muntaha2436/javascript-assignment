69594313a21ed388c1ca5cbfcd31295c10009224
Muntaha2436 <hussainkazium592@gmail.com>
2025-09-08 10:54:09 -0700

    Added assignment files

---
+++ assignment 4/app.js
@@ -0,0 +1,29 @@
+
+// question 1
+
+// let name ="muntaha", age = "15" , city = "karachi"
+
+// question 2
+
+// 5 ligal variabel 
+
+// var myName ;
+// var _age ;
+// var $price;
+// var userOne;
+// var firstName;
+
+// 5 illigal variabel
+
+// var var;
+// var 1name;
+// var @userOne;
+// var !onclass;
+// var me are ;
+
+// question 3
+
+// let rules = "<b> Rules for naming js variable </b> </br>   </br>"
+// let blanks ="b) Variable names can only contain,numbers,$and,_.For example $my_1stVariable</br>c) Variables must begin with a letter,$or_.For example $name, _name or name</br>d) Variable names are case camel and sensitive</br>e) Variable names should not be JS keyword."
+// document.write(rules)
+// document.write(blanks)
