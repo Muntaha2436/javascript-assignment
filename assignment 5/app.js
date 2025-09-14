28514b05bb280820b417fb9797c94e1d00175929
Muntaha2436 <hussainkazium592@gmail.com>
2025-09-14 09:15:03 -0700

    5 assignment done

---
+++ assignment 5/app.js
@@ -0,0 +1,218 @@
+
+
+// question 1
+
+// let num1 = 5
+// let num2 = 3
+// let num3 = num1 + num2
+// let total = `sum of ${num1} and ${num2} is ${num3}`
+// document.write(total)
+ 
+
+// question 2
+
+// let num4 = 5
+// let num5 = 3
+// let num6 = num4 - num5
+// let total2 = `subtrac of ${num4} and ${num5} is ${num6}`
+// document.write(total2)
+
+
+// let value4= 5
+// let value5 = 3
+// let value6 = value4 * value5
+// let final = `multipli of ${value4} and ${value5} is ${value6}`
+// document.write(final)
+
+
+// let num8 = 5
+// let num9 = 3
+// let num2 = num8 / num9 
+// let answer = `division of ${num8} and ${num9} is ${num2}`
+// document.write(answer)
+
+
+// let num4 = 5
+// let num5 = 3
+// let num6 = num4 % num5
+// let total2 = `modulus of ${num4} and ${num5} is ${num6}`
+// document.write(`total2 `)
+
+
+// question 3
+
+// let sum;
+//  document.write(`Value after variable declaration is:   ${sum}  <br>`);
+//    sum = 5;
+//     document.write(`Initial value: ${sum} <br>`);
+//      sum++;
+//         document.write(`Value after increment is:  ${sum} <br>`);
+//          sum = sum + 7;
+//            document.write(`Value after addition is:   ${sum}  <br>`);
+//               sum--;
+//               document.write(`Value after decrement is:  ${sum } <br>`);
+//                 let remainder = sum % 3;
+//                   document.write(`The remainder is :  ${remainder}`);
+
+
+// question 4
+
+// let ticket1 = 600
+// let ticket2 = 5
+// let total = ticket1 * ticket2
+// document.write(`Total cost to buy ${ticket2} tickets to a movie is ${total}PKR`)
+
+
+// question 5
+
+//  let number = 4;
+// document.write("Multiplication Table of " + number + "<br><br>");
+
+//         // Loop to generate table
+//         for (var i = 1; i <= 10; i++) {
+//             document.write(number + " x " + i + " = " + (number * i) + "<br>");
+//         }
+
+
+
+// question 6
+
+
+//  let celsius = 25;
+
+        // b. Convert it to Fahrenheit
+        // let fahrenheitResult = (celsius * 9/5) + 32;
+        // document.write(celsius + "°C is " + fahrenheitResult + "°F<br>");
+
+        // // c. Store a Fahrenheit temperature into a variable
+        // let fahrenheit = 70;
+
+        // // d. Convert it to Celsius
+        // let celsiusResult = (fahrenheit - 32) * 5/9;
+        // document.write(`${fahrenheit}  °F is   ${celsiusResult}  °C`);
+
+
+        // question 7
+
+        // document.write(`<h1>Shopping Cart</h1>`)
+        // let price1 = 650
+        // let price2 = 100
+        // let quantity1 = 3
+        // let quantity2 = 7
+        // let charges = 100
+        // let total = 2750
+        // document.write(`price of item 1 is ${price1} </br> quantity of item 1 is ${quantity1} </br> price of item 2 is ${price2} </br> quantity of item 2 is ${quantity2} </br> shipping charges ${charges} </br> Total cost of your order is ${total}`)
+        
+
+        // question 8
+
+        // let totalMarks = 500;
+        // let obtainedMarks = 423;
+        // let percentage = (obtainedMarks / totalMarks) * 100;
+
+        // document.write(`<h1>Marks Sheet</h1>` )
+
+        // document.write(`Total Marks ${totalMarks} </br>`)
+        //  document.write(`obtainedMarks ${obtainedMarks} </br>`)
+        //   document.write(`Percentage ${percentage.toFixed(2)} %`)
+
+        // question 9
+
+        // Currency values
+//        document.write(`<h1>Currency in PKR</h1>`)
+
+// let dollars = 10;
+// let riyals = 25;
+
+// // Conversion in a single expression
+// let totalPKR = (dollars * 104.80) + (riyals * 28);
+
+// // Show result in browser
+// document.write("Total Currency in PKR = " + totalPKR);
+
+
+// question 10
+
+
+// Initialize variable with some number
+// let num = 7;   // you can change this number
+
+// // Perform arithmetic in a single expression
+// let result = ((num + 5) * 10) / 2;
+
+// // Show result in browser
+// document.write("Initial Number: " + num + "<br>");
+// document.write("Final Result: " + result);
+
+
+// question 11
+
+// document.write(`<h1>Age Calculater</h1>`)
+
+// let currentYear = 2025
+// let birthYear = 2009
+// let age1 = currentYear - birthYear;   // if birthday passed
+// let age2 = age1 - 1
+
+// document.write(`Current year ${currentYear} </br>`)
+// document.write(`birth year ${birthYear} </br>`)
+// document.write(`you Age ${age2}`)
+
+// question  12
+
+// document.write(`<h1>The Geometrizer</h1>`)
+
+// // Store radius into a variable
+// let radius = 20;  
+
+// // π ka value
+// let pi = 3.142;  
+
+// // Circumference calculate (2 * π * r)
+// let circumference = 2 * pi * radius; 
+// document.write(`Radius of circle : ${radius} </br>`) 
+// document.write(`The circumference is : ${circumference} </br>`);
+
+// // Area calculate (π * r^2)
+// let area = pi * radius * radius;  
+// document.write(`The area is : ${area}`);
+
+
+// question 138
+
+// document.write(`<h1>The Lifetime Supply Calculator: </h1>`)
+
+// // a. Store your favorite snack
+// let favoriteSnack = "Chocolate Chip Cookies";
+
+// // b. Store your current age
+// let currentAge = 15;
+
+// // c. Store a maximum age
+// let maxAge = 65;
+
+// // d. Store an estimated amount per day
+// let amountPerDay = 3;
+
+// // e. Calculate total snacks for the rest of life
+// let totalSnacks = maxAge + currentAge * 3 * amountPerDay 
+
+// // Output
+// document.write(`favorite snack : ${favoriteSnack} </br> current Age : ${currentAge}</br> maximum Age : ${maxAge} </br>
+//        Amount of snack per day :${amountPerDay} </br> You will need ${totalSnacks} Chocolate Chip to last you until the ripe old age of ${maxAge}  `)
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        
+
