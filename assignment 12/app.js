785cbfba36986d1d524decd7b3e1295dcdfbc584
Muntaha2436 <hussainkazium592@gmail.com>
2025-10-13 11:18:44 -0700

    added has new file

---
+++ assignment 12/app.js
@@ -0,0 +1,65 @@
+// question 1
+// let num = prompt("Enter a positive integer:");
+
+// num = parseFloat(num);
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+// question 2
+
+// let num = prompt("Enter a negative floating point number:");
+// num = parseFloat(num);
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+// question 3
+
+// let num = prompt("Enter a number:");
+// num = parseFloat(num);
+// let absoluteValue = Math.abs(num);
+// document.write("The absolute value of " + num + " is " + absoluteValue);
+
+// question 4
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice value: " + dice);
+
+// question 5
+
+// let toss = Math.floor(Math.random() * 2) + 1;
+
+// if (toss === 1) {
+//     document.write("Random coin value: Heads");
+// } else {
+//     document.write("Random coin value: Tails");
+// }
+
+// question 6
+
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// document.write("Random number between 1 and 100: " + randomNumber);
+
+// question 7
+
+// let userInput = prompt("Enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+// let weight = parseFloat(userInput);
+// document.write("The weight of the user is " + weight + " kilograms");
+
+// question 8
+
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// let userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+// if (userGuess === secretNumber) {
+//     alert("Congratulations! You guessed the correct number 🎉");
+// } else {
+//     alert("Sorry, the secret number was " + secretNumber + ". Try again!");
+// }
+
